6cafcf945f7ec66d14733a3e439ab8940d289307
siddharth <SiddharthChillale>
2020-03-14 01:28:20 +0530

    changed start time for run_app

--- run_app.py
+++ run_app.py
@@ -9,14 +9,19 @@ now = datetime.now()
 
 
 print("Initiated at = ",now.strftime("%d/%m/%Y %H:%M:%S"))
+print("\n\tRunning script now at...\t", now.strftime("%d/%m/%Y %H:%M:%S"))
+# print("Script run for ", num_of_runs, " times")
+os.system("python corona_vis.py")
+print("\n\tNext Script run in 5 minutes\n")
+
 
 def job():
+
     now = datetime.now()
     print("\n\tRunning script now at...\t", now.strftime("%d/%m/%Y %H:%M:%S"))
     # print("Script run for ", num_of_runs, " times")
-
     os.system("python corona_vis.py")
-    print("\n\tNext Script run in 12 hours\n")
+    print("\n\tNext Script run in 5 minutes\n")
 # schedule.every(1).minutes.do(job)
 schedule.every(5).minutes.do(job)
 
